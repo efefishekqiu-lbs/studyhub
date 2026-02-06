f40850248f3d6a272a5e8323444b9429a3182435
wotex <106171903+wotex3@users.noreply.github.com>
2026-02-06 14:33:51 +0100

    Update panel.js

--- public/js/panel.js
+++ public/js/panel.js
@@ -784,16 +784,12 @@ $(document).on("click", ".burger-menu", function () {
      })
      document.querySelector("nav").innerHTML = `
      <h1 onclick="selectNavbarButton("startsida")" style="padding: 25px">Navigering</h1>
-      <div class="lektioner" data-id="startsida" data-type="selectable" style="margin-top: 10px;">
+        <div class="lektioner" data-id="startsida" data-type="selectable" style="margin-top: 10px;">
         <div class="lektioner" data-id="kalender" data-type="selectable">
         <div> <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M12 192h424c6.6 0 12 5.4 12 12v260c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V204c0-6.6 5.4-12 12-12zm436-44v-36c0-26.5-21.5-48-48-48h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v36c0 6.6 5.4 12 12 12h424c6.6 0 12-5.4 12-12z"></path></svg> </div>
           <h1>Kalender</h1>
-        </div>
-        
-        <div> <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg> </div>
-        <h1>Startsida</h1>
       </div>
-    `
+   `
    
       $(".lektioner>h1").show()
       $(".lektioner>div").show()
